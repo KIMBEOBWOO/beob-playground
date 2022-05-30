d7dd71a6fe7ec2a0bd7532578d2b1c492b47e8d0
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-05-30 11:58:59 +0900

    Feat >>> TEST

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Server Port :::: ' + process.env.SERVER_PORT;
+    return 'SERVER RUNNING ON :::: ' + process.env.SERVER_PORT;
   }
 }
