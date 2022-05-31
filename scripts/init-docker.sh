48a806b087eb55a18c6b78111c0fdda8290890b3
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-05-31 13:36:04 +0900

    Fix >>> Shell scripts : symbolic link 수정

--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -12,6 +12,6 @@ cd /home/ec2-user/beob-playground
 echo "Install docker-compose ..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose -v
 docker-compose down
