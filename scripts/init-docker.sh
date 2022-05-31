d6beb5fd45104a8cb2383ca89f355f873b5adc45
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-05-31 12:03:19 +0900

    Feat >>> init-docker.sh : sudo 쓰면 안대넹

--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -1,5 +1,5 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-sudo docker-compose -v
+docker-compose -v
