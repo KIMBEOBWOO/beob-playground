5405470e6cb49c8b1bc1edbfcf4bb54a9eb21948
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-05-31 13:21:32 +0900

    Fix >>> docker-compose file path 실행 테스트

--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo amazon-linux-extras install docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-
 sudo service docker start
+
 cd /home/ec2-user/beob-playground
 
-docker-compose up -d
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+/usr/local/bin/docker-compose up -d
