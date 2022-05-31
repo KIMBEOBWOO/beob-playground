0954aca2135a403ea4ec0cd56c4b29fef7286792
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-05-31 12:08:46 +0900

    Fix >>> missing docker-compose Error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-docker-compose up -d --build
+#!/bin/sh
+# Remember to change back to ec2-user
+cd /home/ec2-user/server
+
+docker-compose up -d
--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -1,5 +1,11 @@
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+#!/bin/sh
+sudo amazon-linux-extras install docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 
-docker-compose -v
+sudo service docker start
+cd home/ec2-user/beob-playground
+
+docker-compose up -d
