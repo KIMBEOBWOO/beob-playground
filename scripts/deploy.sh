a53d442ccd4846fac2849e17b83892f471755aea
beobwoo <beobwoo@beobwooui-MacBookPro.local>
2022-07-14 16:10:36 +0900

    Feat >>> AWS script file edited

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Start docker containers ..."
-cd /home/ec2-user/beob-playground
+cd /home/ec2-user/server
 docker-compose up -d --build
--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -7,7 +7,7 @@ sudo usermod -a -G docker ec2-user
 sudo service docker start
 
 echo "Go to Working directory ..."
-cd /home/ec2-user/beob-playground
+cd /home/ec2-user/server
 
 echo "Install docker-compose ..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
